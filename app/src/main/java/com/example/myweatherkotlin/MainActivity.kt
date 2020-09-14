9bde90278b74e5ce868e310cf14ed39d367eb9e9
Julia_Anish <julia.md999@gmail.com>
2020-09-14 14:58:47 +0300

    it works

--- app/src/main/java/com/example/myweatherkotlin/MainActivity.kt
+++ app/src/main/java/com/example/myweatherkotlin/MainActivity.kt
@@ -53,6 +53,7 @@ class MainActivity : AppCompatActivity() {
 
                     CITY =cityNew
                     myAlertDialog.dismiss()
+                    weatherTask().execute()
                 }
                 myDialogView.button_cancel.setOnClickListener{
                     myAlertDialog.dismiss()
